c81983db01c53879b947a81e9f2104ee5006ab2a
raphaelrms <raphaelm@gmail.com>
2014-01-12 10:34:42 -0200

    por Raphael Menezes: Reformulacão da view do PermissionRole

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,8 @@ module RailsPadrao
     # don't generate RSpec tests for views and helpers
     config.generators do |g|
       
-      g.test_framework :rspec, fixture: true
-      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.test_framework :rspec, :fixture => true
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
       
       
       g.view_specs false
