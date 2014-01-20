436e82ba69c75473de11dfe10fc389c0965f2701
acarolinatavares <Res!768837>
2014-01-20 16:33:01 -0200

    CLOSES #2 por Ana Carolina: Criação do modelo de fase

--- app/models/fase.rb
+++ app/models/fase.rb
@@ -3,4 +3,6 @@ class Fase < ActiveRecord::Base
   belongs_to :curso
   has_and_belongs_to_many :custos
 
+  attr_accessible :orçamento, :descricao, :curso_id
+
 end
--- db/migrate/20140120181417_create_fases.rb
+++ db/migrate/20140120181417_create_fases.rb
@@ -1,7 +1,9 @@
 class CreateFases < ActiveRecord::Migration
   def change
     create_table :fases do |t|
-
+      t.string :descricao
+      t.integer :curso_id, :null => false, :references => [:cursos, :id]
+      t.float :orçamento
       t.timestamps
     end
   end
