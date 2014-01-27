c40eb0f0042ae769ccffdf1cab614308000f5559
raphaelrms <raphaelm@gmail.com>
2014-01-27 02:16:52 -0200

    por Raphael Menezes: Adaptando edit de curso para poder anexar mais de um arquivo.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,7 @@
 
 function add_fields(link, association, content) {
     var new_id = new Date().getTime();
-    var regexp = new RegExp("new_" + association, "g")
-    $(link).parent().before(content.replace(regexp, new_id));
+    var regexp = new RegExp("new_" + association, "g");
+    debugger;
+    $(link).parent().after(content.replace(regexp, new_id));
 }
--- app/models/curso.rb
+++ app/models/curso.rb
@@ -1,10 +1,10 @@
 class Curso < ActiveRecord::Base
 
-  has_many :arquivos, :as => 'anexo', :dependent => :destroy
+  has_many :arquivos, :as => :anexo, :dependent => :destroy
 
   has_many :fases
 
-  attr_accessible :nome, :descricao, :ano, :arquivos_attributes,:arquivos, :descricao
+  attr_accessible :nome, :descricao, :ano, :descricao, :anexo_attributes,:arquivos_attributes
 
   accepts_nested_attributes_for :arquivos
   #validates :arquivos, :length => { :minimum => 1 }
