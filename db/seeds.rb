85c1103b4a077be4cb86f86cf78f89c9f55637c0
acarolinatavares <acarolinatavaress@gmail.com>
2014-04-03 17:42:13 -0300

    por Ana Carolina: Criando CRUD de Custo

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,6 +22,8 @@ perm = Permission.find_or_create_by_action_and_subject_class :action => 'manage'
 Permission.find_or_create_by_action_and_subject_class_and_subject_id :action => 'show', :subject_class => 'user', :subject_id => 'current_user.id'
 PermissionRole.find_or_create_by_permission_id_and_role_id :permission_id => perm.id, :role_id => user.roles.first
 
+
+
 puts 'Criando categoria "SEM CATEGORIA"'
 categoria = Categoria.find_or_create_by_descricao :descricao => "SEM CATEGORIA"
 puts "Categoria '#{categoria}' criada com sucesso."
