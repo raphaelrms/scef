5eca09a7a870c0bdcdfd7c73dacdc8c6e3991d08
acarolinatavares <acarolinatavaress@gmail.com>
2014-04-03 17:32:50 -0300

    por Ana Carolina: Criando CRUD de Custo

--- db/migrate/20131226184807_add_role_to_permission.rb
+++ db/migrate/20131226184807_add_role_to_permission.rb
@@ -1,6 +1,6 @@
 class AddRoleToPermission < ActiveRecord::Migration
     def change
-      add_column :permissions, :role_id, :integer, :limit => 11
+      add_column :permissions, :role_id, :integer
       add_foreign_key(:permissions,:roles)
     end
 end
