0f6a756c4b3b3acac8a2f357ecd5e2401512877b
raphaelrms <raphaelm@gmail.com>
2014-04-05 13:41:54 -0300

    por Raphael Menezes: Correção da leitura do manage all

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,8 @@ class Ability
         user.roles.each do |role|
           role.permissions.each do |permission|
           if permission.subject_id.nil?
-            can permission.action.to_sym, (permission.subject_class == "all") ? :all : permission.subject_class.constantize
+            binding.pry
+            can permission.action.to_sym, (permission.subject_class.mb_chars.downcase.to_s== "all") ? :all : permission.subject_class.constantize
           else
             can permission.action.to_sym, permission.subject_class.constantize, :id => (permission.subject_id == 0 ? user.id : permission.subject_id )
           end
