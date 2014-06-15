c98b40ea145cbaecde9650a97028821649ae436b
acarolinatavares <Res!768837>
2014-06-15 17:25:49 -0300

    CLOSES #20 por Ana Carolina: Inclusão de atributo virtual para cáculo de valor com INSS

--- app/models/custo.rb
+++ app/models/custo.rb
@@ -7,6 +7,13 @@ class Custo < ActiveRecord::Base
 
   validates :fase,:presence => true
 
-  attr_accessible :valor, :fase_id, :categoria_id, :observacoes, :dt_referencia, :quantidade,:descricao
+  attr_accessible :valor, :fase_id, :categoria_id, :observacoes, :dt_referencia, :quantidade,:descricao, :tributo
+
+  attr_accessor :valor_com_tributacao
+
+  def valor_com_tributacao
+    valor_total = self.valor*self.quantidade
+    return valor_total + (valor_total*self.tributo)/100 unless self.tributo.nil?
+  end
 
 end
