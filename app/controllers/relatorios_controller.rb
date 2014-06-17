1b459cbe1951e0f713fd36fc5a3653e013419435
acarolinatavares <Res!768837>
2014-06-16 22:21:24 -0300

    REFS #21 por Ana Carolina: Geração de Relatórios

--- app/controllers/relatorios_controller.rb
+++ app/controllers/relatorios_controller.rb
@@ -1,7 +1,27 @@
 #encoding: utf-8
 class RelatoriosController < ApplicationController
 
+  def relatorio_fase_custos
+  end
 
+  def exporta_relatorio
+
+
+    respond_to do |format|
+      format.html {
+        if params[:fase_id]==""
+          @fase = Fase.all
+        else
+          @fase = Fase.where(:id=>params[:fase_id])
+        end
+        @custos_associados = Custo.paginate(:page => params[:page], :per_page => 5)
+      }
+      format.pdf do
+        render :pdf => "relatorio.pdf"
+      end
+    end
+
+  end
 
 
 end
--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
@@ -7,6 +8,9 @@ require "action_mailer/railtie"
 require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
+
+require 'wicked_pdf'
+config.middleware.use WickedPdf::Middleware
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
